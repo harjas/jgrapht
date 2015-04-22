e5e681ac736dae6701a2a4a07155dc825a70f9b4
Harjas Singh <hsingh10@cs498dm-23a.cs.illinois.edu>
2015-04-21 23:53:28 -0500

    added 4 cucumber tests

--- jgrapht-core/src/test/java/org/jgrapht/traverse/StepDefinitions.java
+++ jgrapht-core/src/test/java/org/jgrapht/traverse/StepDefinitions.java
@@ -7,13 +7,18 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.And;  
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class StepDefinitions {
 	
 	private DirectedGraph<String, DefaultEdge> dg;
+	private DirectedGraph<String, DefaultEdge> dg2;
+	private DirectedGraph<String, DefaultEdge> dg3;
         private boolean isEmpty;
         private AbstractGraphIterator<String, DefaultEdge> dfs;
-	
+	private AbstractGraphIterator<String, DefaultEdge> dfs2;
+	private AbstractGraphIterator<String, DefaultEdge> dfs3;
+
 	@Given("^There is a graph$")
 	public void thereIsAGraph(){
 		this.dg = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
@@ -55,5 +60,57 @@ public class StepDefinitions {
 
                 String expected = "ABCD";
                 assertEquals(expected, actual);
+	}
+
+	@Given("^an empty graph$")
+	public void emptyGraph(){
+		this.dg2 = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+	}
+
+	@When("^I run DFS$")
+	public void DFSonEmpty(){
+		this.dfs2 = new DepthFirstIterator<String, DefaultEdge>(this.dg2);
+	}
+
+	@Then("^output is fail$")
+	public void outputIs(){
+		assertFalse(this.dfs2.hasNext());
+	}
+
+	@Given("^a graph$")
+	public void thisIsGraph(){
+		this.dg3 = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+		String a = "A";
+		String b = "B";
+		String c = "C";
+	
+		this.dg3.addVertex(a);
+		this.dg3.addVertex(b);
+		this.dg3.addVertex(c);
+
+		this.dg3.addEdge(a,b);
+		this.dg3.addEdge(b,c);
+	}
+
+	@When("^you run dfs$")
+	public void runningDFS(){
+		this.dfs3 = new DepthFirstIterator<String, DefaultEdge>(this.dg3);
+	}
+
+	@Then("^you get output$")
+	public void outputHere(){
+		/*String actual = "";
+       		while (this.dfs3.hasNext()) {
+           		String v = dfs.next();
+            		actual += v;
+        	}
+
+        	String expected = "ABC";*/
+        	assertEquals(true, this.dg3.vertexSet().size() == 3);
+	}
+
+	@Then("^the edges are inserted$")
+	public void outputOfEdges(){
+		assertEquals(true, this.dg3.edgeSet().size() == 2);
 	}
 }
--- jgrapht-core/src/test/resources/org/jgrapht/traverse/dfs.feature
+++ jgrapht-core/src/test/resources/org/jgrapht/traverse/dfs.feature
@@ -5,3 +5,17 @@ Feature: Depth First Search on a graph
 		And graph is not empty
 		When you run DFS
 		Then the output is the path taken
+	
+	Scenario: empty graph test
+		Given an empty graph
+		When I run DFS
+		Then output is fail
+
+	Scenario: check if vertex insertion works
+		Given a graph
+		When you run dfs
+		Then you get output
+
+	Scenario: check if edge insertion works
+		Given a graph
+		Then the edges are inserted
